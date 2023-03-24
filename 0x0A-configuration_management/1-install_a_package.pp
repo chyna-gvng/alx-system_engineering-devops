d46b9febc97eb91985dbbef1e97e72ea933cd69f
Walter <w.angoye@alustudent.com>
2023-03-24 19:50:36 +0300

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,11 @@
 # install 'flask' from 'pip3'
 
-package { 'python3-pip':
-  ensure => installed,
-}
-
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+
+
+package { 'python3-pip':
+  ensure => installed,
 }

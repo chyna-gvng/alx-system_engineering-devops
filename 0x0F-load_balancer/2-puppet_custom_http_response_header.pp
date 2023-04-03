ded5ff294ce8a7d3c98a271b7db82b413eb06b6b
Walter <w.angoye@alustudent.com>
2023-04-03 17:23:30 +0300

    load balancer

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,37 @@
+# puppet code to install & configure 'nginx'
+package { 'nginx':
+  ensure => installed,
+}
+
+file { '/etc/nginx/sites-available/default':
+  content => "server {
+                listen 80 default_server;
+                listen [::]:80 default_server;
+
+                root /var/www/html;
+                index index.html index.htm index.nginx-debian.html;
+
+                server_name _;
+
+                # Custom HTTP header
+                add_header X-Served-By $hostname;
+
+                location / {
+                        try_files $uri $uri/ =404;
+                }
+        }",
+  mode    => '0644',
+  require => Package['nginx'],
+}
+
+file { '/etc/nginx/sites-enabled/default':
+  ensure => 'link',
+  target => '/etc/nginx/sites-available/default',
+  notify => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure     => 'running',
+  enable     => true,
+  subscribe  => File['/etc/nginx/sites-enabled/default'],
+}
